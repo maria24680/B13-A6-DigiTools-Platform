3aa2b7bea374feb06cdf8f731c020d4f1bc79266
maria24680 <mariamila24680@gmail.com>
2026-03-31 19:50:09 +0600

    some issues fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/B13-A6-DigiTools-Platform/",
   plugins: [react(), tailwindcss()],
 })
